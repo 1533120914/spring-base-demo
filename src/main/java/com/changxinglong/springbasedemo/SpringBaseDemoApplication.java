1e2f83aa144a3bba95c167c8282064f15ffba0bb
常兴隆 <133223899+1533120914@users.noreply.github.com>
2024-02-10 16:30:49 +0800

    first commit

---
+++ src/main/java/com/changxinglong/springbasedemo/SpringBaseDemoApplication.java
@@ -0,0 +1,13 @@
+package com.changxinglong.springbasedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBaseDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBaseDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/changxinglong/springbasedemo/SpringBaseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.changxinglong.springbasedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBaseDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
